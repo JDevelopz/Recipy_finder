cafbfaf26978b9835b5aae48cc68f3c353b2de8b
Jeffrey Meijer <jm@Jeffreys-MacBook-Pro.local>
2023-02-07 11:50:05 +0800

    First commit, upload version 1 to Github

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '0d20b7a9-adb8-4742-be32-d56be113a4eb';
+export const MODAL_CLOSE_SEC = 2.5;
